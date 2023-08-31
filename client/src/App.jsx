e3e3ec1d376839ec95dec3a799d54fe2aa80d8e1
= <=>
2023-08-31 13:20:52 +0500

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css'
 // import {Route} from 'react-router-dom'   
-import 
-{ HomeLayout,
-  Landing,
-  Register,
-  Login,
-  DashboardLayout,Error,AddJob,Stats,AllJobs,Profile,Admin,EditJob } from './pages'
+import {
+HomeLayout,
+Landing,
+Register,
+Login,
+DashboardLayout, Error, AddJob, Stats, AllJobs, Profile, Admin, EditJob
+} from './pages'
 
 import { action as registeraction } from './pages/Register';
-import{action as loginaction} from './pages/Login';
+import { action as loginaction } from './pages/Login';
 import { action as addjobaction } from './pages/AddJob';
 import { action as editjobaction } from './pages/EditJob';
 import { action as deletejobaction } from './pages/DeleteJob';
@@ -22,80 +23,80 @@ import { loader as statsloader } from './pages/Stats';
 
 
 const router = createBrowserRouter([
-    {
-      path:'/',
-      element:<HomeLayout/>,
-      errorElement:<Error/>,
-    },
-    {
-      path:'login',
-      element:<Login/>,
-      errorElement:<Error/>,
-      action:loginaction
-    },
-    {
-      path:'register',
-      element:<Register/>,
-      errorElement:<Error/>,
-      action:registeraction
-    },
-    {
-      path:'dashboard',
-      element:<DashboardLayout/>,
-      errorElement:<Error/>,
-      loader:dashboardloader,
-      children:[
-        {
-          index:true,
-          element:<AllJobs />,
-          loader:jobsloader,
-        },
-        {
-          path:'edit-job/:id',
-          element:<EditJob/>,
-          action:editjobaction,
-          loader:editjobloader
-        },
-        {
-          path:'delete-job/:id',
-          action:deletejobaction,
-        },
-        {
-          path: 'stats',
-          element: <Stats />,
-          errorElement: <Error />,
-          loader:statsloader
-        },
-        {
-          path: 'addjob',
-          element: <AddJob />,
-          errorElement: <Error />,
-          action: addjobaction
-        },
-        {
-          path: 'profile',
-          element: <Profile />,
-        },
-        {
-          path: 'admin',
-          element: <Admin />,
-          loader:adminloader,
-        },
-      ]
-    },
-    {
-      path:'landing',
-      element:<Landing/>,
-      errorElement:<Error/>,
-    }
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: 'login',
+    element: <Login />,
+    errorElement: <Error />,
+    action: loginaction
+  },
+  {
+    path: 'register',
+    element: <Register />,
+    errorElement: <Error />,
+    action: registeraction
+  },
+  {
+    path: 'dashboard',
+    element: <DashboardLayout />,
+    errorElement: <Error />,
+    loader: dashboardloader,
+    children: [
+      {
+        index: true,
+        element: <AllJobs />,
+        loader: jobsloader,
+      },
+      {
+        path: 'edit-job/:id',
+        element: <EditJob />,
+        action: editjobaction,
+        loader: editjobloader
+      },
+      {
+        path: 'delete-job/:id',
+        action: deletejobaction,
+      },
+      {
+        path: 'stats',
+        element: <Stats />,
+        errorElement: <Error />,
+        loader: statsloader
+      },
+      {
+        path: 'addjob',
+        element: <AddJob />,
+        errorElement: <Error />,
+        action: addjobaction
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+      {
+        path: 'admin',
+        element: <Admin />,
+        loader: adminloader,
+      },
+    ]
+  },
+  {
+    path: 'landing',
+    element: <Landing />,
+    errorElement: <Error />,
+  }
 
-  ])
-  
+])
+
 export default function App() {
 
   return (
     <div>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </div>
   )
 }
@@ -110,21 +111,21 @@ export default function App() {
 
 
 
-    // <div>
-      {/* <Routes>
+// <div>
+{/* <Routes>
         <Route exact path='/' element={<HomeLayout/>}></Route>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' action={() => {console.log('hello there')}} element={<Register/>}/> */}
 
-        {/* Nested Routes  for Dashboard Pages*/}
-        {/* <Route exact path='/dashboard/*' element={<DashboardLayout/>}/> 
+{/* Nested Routes  for Dashboard Pages*/ }
+{/* <Route exact path='/dashboard/*' element={<DashboardLayout/>}/> 
           */}
-         {/* <Route path='admin' element={<Admin/>}/> */}
-        {/* </Route> */}
+{/* <Route path='admin' element={<Admin/>}/> */ }
+{/* </Route> */ }
 
-        {/* Landing Page */}
-        {/* <Route exact path='/landing' element={<Landing/>}/>
+{/* Landing Page */ }
+{/* <Route exact path='/landing' element={<Landing/>}/>
         <Route path='*' element={<Error/>}/>
       </Routes> */}
-    {/* </div> */}
+{/* </div> */ }
 
